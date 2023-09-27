8e5ec68fd8339172b2a7d3badc8e6c574f2bb99f
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-09-27 17:10:29 +0530

    responsive cards added with mapping the card datas

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import HeroSection from './components/HeroSection'
+import HeroCards from './components/HeroCards'
+import HeroSection from './components/heroSection'
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <HeroCards />
     </main>
   )
 }
