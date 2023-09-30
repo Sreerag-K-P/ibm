d166ce7484b264bc93663a41f8c15fc18c5456f1
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-09-30 23:11:49 +0530

    meetglobal added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Cardgroup } from './components/Cardgroup'
 import FeatureStory from './components/FeatureStory'
 import HeroCards from './components/HeroCards'
 import Introduction from './components/Introduction'
+import MeatOurGlobal from './components/MeatOurGlobal'
 import HeroSection from './components/heroSection'
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Introduction />
       <Cardgroup />
       <FeatureStory />
+      <MeatOurGlobal />
       <div className="h-[100vh]"></div>
     </main>
   )
